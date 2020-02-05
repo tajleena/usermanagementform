19a379539cbef0c3ee4a8ad8c1a244c444e8ca82
tajleena <tajleena15@gmail.com>
2020-02-05 19:44:54 +0530

    2 files modified

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,36 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import {User} from '../user-data.model';
+import { Component, OnInit } from "@angular/core";
+import { User } from "../user-data.model";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
 export class UserComponent implements OnInit {
-  userData=User;
+  userData = User;
 
-  constructor() { }
-  arrUser=[];
+  constructor() {}
+  arrUser = [];
   ngOnInit() {
-     for(let i =0; i<localStorage.length;i++){
+    for (let i = 0; i < localStorage.length; i++) {
       let key = localStorage.key(i);
       let value = localStorage.getItem(key);
-        // console.log(value); 
+      // console.log(value);
 
-
-
-        this.arrUser.push({id:JSON.parse(localStorage.getItem(key)).id,name:JSON.parse(localStorage.getItem(key)).name , emailId:JSON.parse(localStorage.getItem(key)).emailId,password:JSON.parse(localStorage.getItem(key)).password})
-            
-
-      
-      
+      this.arrUser.push({
+        id: JSON.parse(localStorage.getItem(key)).id,
+        name: JSON.parse(localStorage.getItem(key)).name,
+        emailId: JSON.parse(localStorage.getItem(key)).emailId,
+        password: JSON.parse(localStorage.getItem(key)).password
+      });
     }
- 
-  
   }
-     delete(index)
-{
-  this.userData.splice(index,1);
-}
-
+  delete(index) {
+    this.arrUser.splice(index, 1);
+    
+  localStorage.removeItem(index);
+  }
 }
